dedade125958944cd8f55bec08c8e2cd664ff2f2
Nick <44916311+nzandrulewicz@users.noreply.github.com>
2024-04-20 22:37:07 -0400

    3 and 4 - Tilesets and Parallax

--- Platformer/Platformer/obj/Debug/net6.0/Platformer.AssemblyInfo.cs
+++ Platformer/Platformer/obj/Debug/net6.0/Platformer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e70f504e53356a566d27f9f16e21cea0f90f8a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4718b2cbcc37626f3b294311376497ffe7fce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
